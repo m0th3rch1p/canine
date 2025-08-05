c9e860d0f346f44e9e2c8c64949bafc3c81b4530
Chris <chris@example.com>
2025-08-05 13:30:08 -0700

    fix lints

--- spec/system/docker_compose_health_spec.rb
+++ spec/system/docker_compose_health_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Docker Compose Health Check", type: :system do
   it "successfully loads the application homepage" do
     visit root_path
-    
+
     # The application should be running and accessible
     # Check that we get a page with content (not an error)
     expect(page).to have_css('body')
@@ -11,11 +11,11 @@ RSpec.describe "Docker Compose Health Check", type: :system do
 
   it "renders the application layout" do
     visit root_path
-    
+
     # Check for basic application structure
     # This will vary based on whether user is logged in or not
     expect(page).to have_css('body')
-    
+
     # Check that no error pages are shown
     expect(page).not_to have_content('We\'re sorry, but something went wrong')
     expect(page).not_to have_content('The page you were looking for doesn\'t exist')
@@ -25,7 +25,7 @@ RSpec.describe "Docker Compose Health Check", type: :system do
     it "responds to health check requests" do
       # Direct HTTP check for health endpoint if one exists
       visit root_path
-      
+
       # Verify the page loads without Rails errors
       expect(page).not_to have_content('ActionController::RoutingError')
       expect(page).not_to have_content('NoMethodError')
