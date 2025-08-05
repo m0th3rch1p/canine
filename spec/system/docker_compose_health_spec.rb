bfb9e71a68eac33639ff12c01b01a8a931a5aee6
Chris <chris@example.com>
2025-08-05 13:38:01 -0700

    added credentials spec

--- spec/system/docker_compose_health_spec.rb
+++ spec/system/docker_compose_health_spec.rb
@@ -3,33 +3,14 @@ require 'rails_helper'
 RSpec.describe "Docker Compose Health Check", type: :system do
   it "successfully loads the application homepage" do
     visit root_path
+    expect(page).to have_css('body')
 
-    # The application should be running and accessible
-    # Check that we get a page with content (not an error)
+    visit rails_health_check_path
     expect(page).to have_css('body')
   end
 
-  it "renders the application layout" do
+  it "can add new credentials" do
     visit root_path
-
-    # Check for basic application structure
-    # This will vary based on whether user is logged in or not
-    expect(page).to have_css('body')
-
-    # Check that no error pages are shown
-    expect(page).not_to have_content('We\'re sorry, but something went wrong')
-    expect(page).not_to have_content('The page you were looking for doesn\'t exist')
-  end
-
-  context "when accessing the application" do
-    it "responds to health check requests" do
-      # Direct HTTP check for health endpoint if one exists
-      visit root_path
-
-      # Verify the page loads without Rails errors
-      expect(page).not_to have_content('ActionController::RoutingError')
-      expect(page).not_to have_content('NoMethodError')
-      expect(page).not_to have_content('NameError')
-    end
+    expect(page).to have_text('Please go to Github.com and enter your personal access token below.')
   end
 end
